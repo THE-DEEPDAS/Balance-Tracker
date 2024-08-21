47f6fb0441822de69f4cd4edeb28b0b2ea29ddac
Deep Das <deepdblm@gmail.com>
2024-08-21 21:18:03 +0530

    final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import SignUp from './SignUp';
 import Login from './Login';
 import BalanceTracker from './BalanceTracker';
+import Navbar from './Navbar';
 import './main.css';
 
 function App() {
@@ -18,20 +19,21 @@ function App() {
 
   return (
     <div className="App">
+      <Navbar /> {/* Add Navbar here */}
+
       {!currentUser ? (
         <div className="auth-container">
           {showLogin ? (
             <>
               <Login setCurrentUser={setCurrentUser} />
-              <p style={{color:"white"}}>
-                New user? <span className="toggle-link" onClick={() => setShowLogin(false)} >Sign up here</span>
+              <p style={{ color: "white" }}>
+                New user? <span className="toggle-link" onClick={() => setShowLogin(false)}>Sign up here</span>
               </p>
             </>
           ) : (
             <>
               <SignUp />
-              <p style={{color:"white"
-              }}>
+              <p style={{ color: "white" }}>
                 Already have an account? <span className="toggle-link" onClick={() => setShowLogin(true)}>Login here</span>
               </p>
             </>
